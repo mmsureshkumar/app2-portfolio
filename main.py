49ceb75da8641728a68466f2aa6a7d4567b0be4a
smurugas <mmsureshkumar@gmail.com>
2024-08-01 19:31:52 +0530

    added app descriptions, images and links

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, empty_col, col2 = st.columns([1.5, 0.5, 1.5])
 
 with col1:
     st.image("images\Suresh.jpg")
@@ -26,8 +26,13 @@ ds = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in ds[:10].iterrows():
         st.header(row['title'])
-
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in ds[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
